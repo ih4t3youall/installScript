373931bac41d36b20263ef2bd5dc48a804e39d32
martin lequerica <ih4t3youall@gmail.com>
2023-03-21 16:49:09 -0300

    adding ohmyzsh

--- install.sh
+++ install.sh
@@ -1,13 +1,26 @@
-if [ 'root' == $(whoami) ]; then
+if [ "root" == $(whoami) ]; then
+	echo "i am root"
 	apt install vim -y
 	apt install curl -y
 	apt install atom -y
+	apt-get install zsh -y
+	echo "if it ask press 2"
+	chsh -s /usr/bin/zsh
+	echo "install oh-my-zsh"
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 else
+	echo "as a mortal user"
 	sudo apt-get install vim -y
 	sudo apt-get install curl -y
 	sudo apt-get install atom -y
 	sudo apt-get install xclip -y
+	sudo apt-get install zsh -y
+	echo "if it ask press 2"
+	chsh -s /usr/bin/zsh
+	echo "install oh-my-zsh"
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
+
 
 echo "create scripts folder"
 mkdir ~/.scripts
@@ -70,28 +83,28 @@ cd
 
 #aliases
 echo "creating aliases"
-echo 'alias downloads="cd ~/Downloads"' >> ~/.bashrc
-echo 'alias documents="cd ~/Documents"' >> ~/.bashrc
-echo 'alias workdir="cd ~/Documents/workspace"' >> ~/.bashrc
-echo 'alias desktop="cd ~/Desktop"' >> ~/.bashrc
-echo 'alias bashrc="vim ~/.bashrc"' >> ~/.bashrc
-echo 'alias bashrcu=". ~/.bashrc"' >> ~/.bashrc
-echo 'alias notes="vim ~/.scripts/notes"' >> ~/.bashrc
-echo 'alias catnotes="cat ~/.scripts/notes"' >> ~/.bashrc
-echo 'alias work="vim ~/.scripts/work"' >> ~/.bashrc
-echo 'alias catwork="cat ~/.scripts/work"' >> ~/.bashrc
-echo 'alias vimrc="vim ~/.vimrc"' >> ~/.bashrc
-echo 'alias mkill="xkill;exit"' >> ~/.bashrc
-echo 'alias draftMessage="vim ~/.scripts/helpers/draftMessage.txt"' >> ~/.bashrc
-echo 'alias copyMessage="cat ~/.scripts/helpers/draftMessage.txt | xclip"' >> ~/.bashrc
+echo 'alias downloads="cd ~/Downloads"' >> ~/.zshrc
+echo 'alias documents="cd ~/Documents"' >> ~/.zshrc
+echo 'alias workdir="cd ~/Documents/workspace"' >> ~/.zshrc
+echo 'alias desktop="cd ~/Desktop"' >> ~/.zshrc
+echo 'alias bashrc="vim ~/.zshrc"' >> ~/.zshrc
+echo 'alias bashrcu=". ~/.zshrc"' >> ~/.zshrc
+echo 'alias notes="vim ~/.scripts/notes"' >> ~/.zshrc
+echo 'alias catnotes="cat ~/.scripts/notes"' >> ~/.zshrc
+echo 'alias work="vim ~/.scripts/work"' >> ~/.zshrc
+echo 'alias catwork="cat ~/.scripts/work"' >> ~/.zshrc
+echo 'alias vimrc="vim ~/.vimrc"' >> ~/.zshrc
+echo 'alias mkill="xkill;exit"' >> ~/.zshrc
+echo 'alias draftMessage="vim ~/.scripts/helpers/draftMessage.txt"' >> ~/.zshrc
+echo 'alias copyMessage="cat ~/.scripts/helpers/draftMessage.txt | xclip"' >> ~/.zshrc
 #open
 echo "installing open"
-echo 'alias open=". ~/.scripts/open/open.sh"' >> ~/.bashrc
+echo 'alias open=". ~/.scripts/open/open.sh"' >> ~/.zshrc
 #slideshow
 echo "installing slideshow"
-echo 'alias slideshow="java -jar ~/.scripts/slideshow/target/randomSlideshow-0.0.1-SNAPSHOT.jar"' >> ~/.bashrc
+echo 'alias slideshow="java -jar ~/.scripts/slideshow/target/randomSlideshow-0.0.1-SNAPSHOT.jar"' >> ~/.zshrc
 echo "installing vim-mode-plus for atom"
 apm install vim-mode-plus
 echo "removing temp folder"
-. ~/.bashrc
+. ~/.zshrc
 rm -rf ~/temp
